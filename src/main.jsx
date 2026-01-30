09ad1b1f626dc80be2826459760c7c636871bf62
unknown <lvashisth520@gmail.com>
2026-01-30 17:38:59 +0530

    Comment added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// this is a comment
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
